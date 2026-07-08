029efa115f92ee0805973132ab4906e131b20dcc
Sara Souza <sarahsouzaph@gmail.com>
2026-07-08 19:51:46 -0300

    Refatoração na função listar_tarefas e adicionar_tarefa

--- projeto_2.py
+++ projeto_2.py
@@ -2,33 +2,17 @@
 import os
 import json
 
+# Atribuindo nome de variável no nosso arquivo json
+ARQUIVO_DE_TAREFAS = "tarefas.json"
 
-# Definindo a nossa função ADCIONAR_TAREFAS; Parâmetros (Convertidos em string)
-def adicionar_tarefa(x: str): 
-    # Tranferir a tarefa recebida para o dicioário, para receber status
-    dict_tarefa = {
-        'tarefa': x,
-        'esta_feita': False
-    }
-    # Permitir que novas tarefas sejam adcionadas em formato de dicionário
-    tarefas.append(dict_tarefa)
-
-# Definindo a função LISTAR_TAREFAS
-def listar_tarefas():
-    #Iterando a LISTA para buscar cada elemento dela
-    for tarefa in tarefas:
-        print(tarefa)
-        #Para cada tarefa, itere os COMPONENTES do DICIONÁRIO
-        for chave, valor in tarefa.items():
-            # SE a chave do dict for uma str ENTAO imprima.
-            if chave == "tarefa":
-                print(valor)
+# Crindo lista p/ adcionar tarefa
+tarefas = [] 
 
 # Definindo a função para persistir os dados recebidos
-def salvar_tarefa(tarefas):
+def salvar_tarefa(x):
     with open(ARQUIVO_DE_TAREFAS, "w", encoding="utf-8") as file:
         #Salvando a lista de tarefas como JSON formatado
-        json.dump(tarefas, file, ensure_ascii=False, indent=4)
+        json.dump(x, file, ensure_ascii=False, indent=4)
 
 # Verificando se há tarefas existentes
 def carregar_tarefas():
@@ -46,35 +30,60 @@ def carregar_tarefas():
     except:
         print('Algo Correu errado.')
 
-# Crindo lista p/ adcionar tarefa
-tarefas=[]
+# Função de adicinar tarefas (refatorada)
+def adicionar_tarefa(): 
+
+    tarefa = input("Digite aqui sua tarefa:")
+
+    # Tranferir a tarefa recebida para o dicioário, para receber status
+    dict_tarefa = {
+        'tarefa': tarefa,
+        'esta_feita': False
+    }
+    # Permitir que novas tarefas sejam adcionadas em formato de dicionário
+    tarefas.append(dict_tarefa)
+
+    # Colocando o input no arquivo json
+    salvar_tarefa(tarefas)
+
+    print(f"Tarefa '{tarefa}' adicionada com sucesso! ✅")
+
+# Definindo a função LISTAR_TAREFAS (Alterações)
+def listar_tarefas():
+    # Listando as tarefas com o enumerate
+    print("\n 📎Lista de Tarefas: ")
+
+    for i, tarefa in enumerate(tarefas, start=1):
+        print(f"{i} - {tarefa['tarefa']}")
+
+
 
 # Iniciando a rotina principal
 print("Bem vindo ao gerenciador de tarefas")
 while True: 
-    # Verificando se já tem tarefas
-    tarefas = carregar_tarefas
+    tarefas = carregar_tarefas()
 
-    print("---------- Gerenciador de Tarefas ---------- ")
+    print("---------- 📎 Gerenciador de Tarefas 📎 ----------\n")
 
     print("1 - Adicionar Tarefa")
     print("2 - Listar tarefas existentes")
-    print("3 - Finalize uma tarefa")
-    print("4 - fechar o programa")
+    print("3 - Finalize uma tarefa (Marcar como concluída)")
+    print("4 - Fechar o programa\n")
  
-    try:
-        opcao = int(input("---------- Digite a opção desejada: ----------"))
-
-    except:
-        print("Opção Inválida. Por favor, digite um número!")
-        
+    opcao = int(input("---------- Digite a opção desejada: ---------- "))
+    
     if opcao == 1:
         adicionar_tarefa()
     
     elif opcao == 2:
         listar_tarefas()
 
+    #Função ainda não criada
     elif opcao == 3:
-        finalizar_tarefa(
+        finalizar_tarefa()
+    
+    elif opcao == 4:
+        print("Saindo do programa")
         break
-        )    
+    else:
+        print("Opção Inválida. Por favor, digite um número!")
